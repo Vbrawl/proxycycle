e6d5dc244f89a4557c3e84695f33698dddaf4b11
Vbrawl <greninjagr@gmail.com>
2024-04-18 13:32:30 +0300

    Added fromString and toString functions.

--- src/proxycycle/Proxy.py
+++ src/proxycycle/Proxy.py
@@ -28,3 +28,70 @@ class Proxy:
     
     def __hash__(self) -> int:
         return hash((self.host, self.port,))
+    
+    @classmethod
+    def fromString(cls, string:str) -> Proxy:
+        """Construct a Proxy object from the given string.
+        Valid formats for the string:
+        * scheme://host:port
+        * host:port
+        * scheme://[IPv6]:port
+        * [IPv6]:port
+
+        Args:
+            string (str): The host string to parse.
+
+        Raises:
+            ValueError: When a component from the string is not valid.
+
+        Returns:
+            Proxy: A Proxy object containing information from the string.
+        """
+        scheme = Scheme.Undefined
+        host:str = ""
+        port:int = -1
+
+        # Supported format: scheme://host:port
+        # Supported format (IPv6): scheme://[IPv6]:port
+        if "://" in string:
+            temp, string = string.split('://', maxsplit=1)
+            try:
+                scheme = {
+                    "http": Scheme.HTTP,
+                    "socks4": Scheme.SOCKS4,
+                    "socks5": Scheme.SOCKS5
+                }[temp.lower()]
+            except KeyError:
+                raise ValueError(f"scheme({temp}) doesn't seem to be a valid scheme.")
+        
+        if string[0] == '[':
+            host, string = string.split(']', maxsplit=1)
+        elif ":" in string:
+            host, string = string.split(':', maxsplit=1)
+        else:
+            host = string
+        
+        if string != '':
+            try:
+                port = int(string)
+            except ValueError:
+                raise ValueError(f"port({string}) doesn't seem to be a valid port.")
+        
+        if host != '' and port >= 0:
+            return Proxy(host, port, scheme)
+        else:
+            raise ValueError(f"string({string}) doesn't seem to be a valid proxy server.")
+    
+    def toString(self) -> str:
+        string = f"{self.host}:{self.port}"
+        try:
+            schemeName = {
+                Scheme.HTTP: "http",
+                Scheme.SOCKS4: "socks4",
+                Scheme.SOCKS5: "socks5"
+            }[self.scheme]
+            string = schemeName + '://' + string
+        except KeyError:
+            pass # Silent
+        
+        return string
--- tests/Proxy.py
+++ tests/Proxy.py
@@ -1,5 +1,6 @@
 import unittest
 from proxycycle.Proxy import Proxy
+from proxycycle.enums.scheme import Scheme
 
 
 class TestProxy(unittest.TestCase):
@@ -21,6 +22,25 @@ class TestProxy(unittest.TestCase):
         self.assertEqual(hash(self.proxies[0]), hash(self.proxies[0]), "hash(proxy) != hash(proxy)?????")
         self.assertNotEqual(hash(self.proxies[0]), hash(self.proxies[1]), "hash(proxy) != hash(other_proxy)?????")
 
+    def test_string_functions(self):
+        p1string = "http://127.0.0.1:8080"
+        p1 = Proxy("127.0.0.1", 8080, Scheme.HTTP)
+        p2string = "socks4://127.0.0.1:8080"
+        p2 = Proxy("127.0.0.1", 8080, Scheme.SOCKS4)
+        p3string = "127.0.0.1:8080"
+        p3 = Proxy("127.0.0.1", 8080)
+
+        self.assertEqual(Proxy.fromString(p1string), p1, "fromString didn't return a correct object")
+        self.assertEqual(Proxy.fromString(p2string), p2, "fromString didn't return a correct object")
+        self.assertEqual(Proxy.fromString(p3string), p3, "fromString didn't return a correct object")
+
+        self.assertEqual(p1.toString(), p1string, "toString didn't return a correct string representation")
+        self.assertEqual(p2.toString(), p2string, "toString didn't return a correct string representation")
+        self.assertEqual(p3.toString(), p3string, "toString didn't return a correct string representation")
+
+        self.assertEqual(Proxy.fromString(p1.toString()), p1, "fromString(toString()) didn't return a correct copy of the object")
+        self.assertEqual(Proxy.fromString(p2.toString()), p2, "fromString(toString()) didn't return a correct copy of the object")
+        self.assertEqual(Proxy.fromString(p3.toString()), p3, "fromString(toString()) didn't return a correct copy of the object")
 
 
 
